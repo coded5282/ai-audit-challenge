a40715c37b66eff76b6370e2a35715ec148d78bc
Ed Chen <edjchen@hyperturing2.stanford.edu>
2022-09-05 12:50:30 -0700

    added data file and protected categories list

---
+++ streamlit-app/data.py
@@ -0,0 +1,10 @@
+PROTECTED_CATEGORIES_DICT = {
+    'Race': ['White', 'Black'],
+    # 'color': [],
+    # 'religion': [],
+    'Sex': ['Male', 'Female'],
+    # 'national origin': [],
+    'Age': ['Young', 'Old'],
+    # 'disability': [],
+    # 'genetic information': []
+}
--- streamlit-app/main_app.py
+++ streamlit-app/main_app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import data as data
 
 print("Starting new page")
 
-st.header("AI Audit Challenge")
+st.set_page_config(layout="wide")
+
+st.title("Protected Categories")
+
+print(data.PROTECTED_CATEGORIES_DICT)
+
+col1, col2, col3 = st.columns((2, 1, 1))
+
+# Display the protected groups/subgroups
+with col1:
+    for group, subgroups in data.PROTECTED_CATEGORIES_DICT.items():
+        st.write(group)
+        for subgroup in subgroups:
+            st.checkbox(subgroup)
