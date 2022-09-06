a59ae2701876b6728ed6e2aef20121a4f329e36f
Ed Chen <edjchen@hyperturing2.stanford.edu>
2022-09-06 11:14:19 -0700

    Created backbone for report page

--- streamlit-app/page_audit_report.py
+++ streamlit-app/page_audit_report.py
@@ -8,6 +8,17 @@ def back_to_settings_on_click():
 def page_audit_report():
     st.title('AI Audit Report')
 
-    # TODO: Finish rest of report interface
+    selected_groups_list = list(st.session_state.protected_groups.keys())
+    tabs_list = st.tabs(selected_groups_list)
+    for tab_idx, curr_tab in enumerate(tabs_list):
+        curr_group = selected_groups_list[tab_idx]
+        num_subgroups = len(st.session_state.protected_groups[curr_group])
+        curr_subgroups = list(st.session_state.protected_groups[curr_group].keys())
+        curr_tab_col_lengths = [1 for _ in range(num_subgroups)]
+        curr_tab_cols = curr_tab.columns(tuple(curr_tab_col_lengths))
+        for curr_tab_col_idx, curr_tab_col in enumerate(curr_tab_cols):
+            curr_tab_col.write('{}'.format(curr_subgroups[curr_tab_col_idx]))
+            curr_tab_col.metric(label='Score', value=0.0)
+            curr_tab_col.write('Concepts')
 
     st.button('Back To Settings', on_click=back_to_settings_on_click)
--- streamlit-app/page_perform_audit.py
+++ streamlit-app/page_perform_audit.py
@@ -8,6 +8,7 @@ def go_to_report_on_click():
     st.session_state.current_page = 'audit_report'
 
 def curr_tab_next_on_click(curr_group):
+    # TODO: This should modify the prompts and scores for the current group
     pass
 
 # Page display function
@@ -21,14 +22,16 @@ def page_perform_audit():
         curr_group = selected_groups_list[tab_idx]
         # the number of subgroups for the current group represents the number of prompts to display
         num_prompts = len(st.session_state.protected_groups[curr_group])
+        col1, col2, col3 = curr_tab.columns((2, 0.5, 0.5))
         for i in range(num_prompts):
-            col1, col2, col3 = curr_tab.columns((2, 0.5, 0.5))
             # TODO: Replace with generated prompts
-            col1.write("Prompt {}".format(i))
-            col2.metric(label='Automated Score', value=0.0)
-            curr_rank = col3.selectbox('Rank', [1, 2], key='rank_{}_{}'.format(i, curr_group))
+            with st.container():
+                col1.write("Prompt {}".format(i))
+                col2.metric(label='Automated Score', value=0.0)
+                curr_rank = col3.selectbox('Rank', [1, 2], key='rank_{}_{}'.format(i, curr_group))
         
-        curr_tab.button('Next', on_click=curr_tab_next_on_click, args=(curr_group,))
+        col3.button('Next', key='next_{}'.format(curr_group), on_click=curr_tab_next_on_click, args=(curr_group,))
 
-    st.button('Back To Settings', on_click=back_to_settings_on_click)
-    st.button('Go To Report', on_click=go_to_report_on_click)
+    page_col1, page_col2 = st.columns((2, 0.40))
+    page_col1.button('Back To Settings', on_click=back_to_settings_on_click)
+    page_col2.button('Go To Report', on_click=go_to_report_on_click)
