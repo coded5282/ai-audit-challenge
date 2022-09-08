4a329135eb31f885eae59922ab6d393264e69b2f
Edward Chen <edwardchen@DNa80073a.SUNet>
2022-09-08 12:38:59 -0700

    Sorted prompts by disparity

--- streamlit-app/data.py
+++ streamlit-app/data.py
@@ -30,9 +30,61 @@ EVALUATION_CONCEPTS_DICT = {
 }
 
 def load_prompt_data():
-    with open(f'../results_final_basketball_.pkl', 'rb') as f:
+    # with open(f'../results_final_basketball_.pkl', 'rb') as f:
+    #     data = pickle.load(f)
+
+    with open(f'../results_final_swimming_.pkl', 'rb') as f:
         data = pickle.load(f)
-    return data
+
+    prompts_core = []
+    prompts_grp_1 = []
+    prompts_grp_2 = []
+    responses_grp_1 = [] 
+    responses_grp_2 = []
+    toxicities_grp_1 = []
+    toxicities_grp_2 = []
+    disparities = []
+
+    for dct in data:
+        prompts_grp_1.append(dct['prompt_text_group_1'])
+        prompts_grp_2.append(dct['prompt_text_group_2'])
+        prompts_core.append(dct['prompt_text_group_1'].lower().replace('china', '[ COUNTRY ]'))
+        responses_grp_1.append(dct['top_response_group_1'])
+        responses_grp_2.append(dct['top_response_group_2'])
+        toxicities_grp_1.append(dct['toxicity_group_1'])
+        toxicities_grp_2.append(dct['toxicity_group_2'])
+        disparities.append(abs(toxicities_grp_1[-1]-toxicities_grp_2[-1]))
+    
+    assert len(prompts_grp_1) == len(prompts_grp_2) == len(prompts_core) == len(responses_grp_1) == len(responses_grp_2) == len(toxicities_grp_1) == len(toxicities_grp_2) == len(disparities)
+
+    # sort by disparity scores
+    objects = list(zip(
+        prompts_core, 
+        prompts_grp_1, 
+        prompts_grp_2, 
+        responses_grp_1, 
+        responses_grp_2, 
+        toxicities_grp_1, 
+        toxicities_grp_2, 
+        disparities
+    ))
+
+    objects = sorted(objects, key = lambda x: x[-1], reverse=True)
+
+    sorted_data = []
+    for each_object in objects:
+        each_dict = {}
+        each_dict['prompts_core'] = each_object[0]
+        each_dict['prompts_grp_1'] = each_object[1]
+        each_dict['prompts_grp_2'] = each_object[2]
+        each_dict['top_response_group_1'] = each_object[3]
+        each_dict['top_response_group_2'] = each_object[4]
+        each_dict['toxicity_group_1'] = each_object[5]
+        each_dict['toxicity_group_2'] = each_object[6]
+        each_dict['disparity'] = each_object[7]
+        sorted_data.append(each_dict)
+
+    return sorted_data
 
 def calculate_subgroup_scores():
     if ('user_ranks' not in st.session_state) or ('protected_groups' not in st.session_state):
