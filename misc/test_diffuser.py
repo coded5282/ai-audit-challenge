7153d25f0f69b89a03ab141942c5df8b6f9fa3d3
Xuechen Li <lxuechen@gmail.com>
2022-10-15 16:13:11 -0700

    Fix.

--- misc/test_diffuser.py
+++ misc/test_diffuser.py
@@ -2,15 +2,18 @@
 #   python misc/test_diffuser.py
 from diffusers import StableDiffusionPipeline
 
-model_id = "CompVis/stable-diffusion-v1-3"
+model_id = "CompVis/stable-diffusion-v1-1"
+# model_id = "CompVis/stable-diffusion-v1-3"
 
 with open(".hf_token", "r") as f:
     access_token = f.readline().strip()
 
 # load model and scheduler
 ldm = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
-num_params = sum(param.numel() for param in ldm.model.parameters())
-print(f'number of params: {num_params}')
+modules = [ldm.unet, ldm.vae, ldm.text_encoder]
+num_params = sum(
+    param.numel() for module in modules for param in module.parameters()
+)
 
 # run pipeline in inference (sample random noise and denoise)
 prompt = "A painting of a squirrel eating a burger"
