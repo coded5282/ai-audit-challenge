e6e68586eb0fec76ce9b57794582a5d3ad74c717
Xuechen Li <lxuechen@gmail.com>
2022-10-15 15:56:44 -0700

    Update.

--- misc/test_diffuser.py
+++ misc/test_diffuser.py
@@ -1,24 +1,16 @@
 # Run this file from root to test inference the models.
 #   python misc/test_diffuser.py
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionPipeline
 
-# Other public options are `CompVis/ldm-text2im-large-256`, `CompVis/ldm-celebahq-256`
-# Other private options are
-#   `CompVis/stable-diffusion-v1-1-diffusers`
-#   `CompVis/stable-diffusion-v1-2-diffusers`,
-#   `CompVis/stable-diffusion-v1-3-diffusers`,
-model_id = "CompVis/stable-diffusion-v1-3-diffusers"
+model_id = "CompVis/stable-diffusion-v1-3"
 
-# To use the stable diffusion private models
-#   1. accept being invited into their CompVis org; details in email thread.
-#   2. create access token here https://huggingface.co/settings/tokens
-#   3. copy access token into ./.hf_token.
-# The above steps are a must to use the really good models, since they aren't fully public!
 with open(".hf_token", "r") as f:
     access_token = f.readline().strip()
 
 # load model and scheduler
-ldm = DiffusionPipeline.from_pretrained(model_id, use_auth_token=access_token)
+ldm = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
+num_params = sum(param.numel() for param in ldm.model.parameters())
+print(f'number of params: {num_params}')
 
 # run pipeline in inference (sample random noise and denoise)
 prompt = "A painting of a squirrel eating a burger"
