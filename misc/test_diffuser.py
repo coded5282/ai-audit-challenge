9bd9c8a3cdcc0f54349bdf386cc03bb4e21a16b4
Xuechen Li <lxuechen@gmail.com>
2022-10-15 16:16:57 -0700

    Check params.

--- misc/test_diffuser.py
+++ misc/test_diffuser.py
@@ -11,9 +11,15 @@ with open(".hf_token", "r") as f:
 # load model and scheduler
 ldm = StableDiffusionPipeline.from_pretrained(model_id, use_auth_token=True)
 modules = [ldm.unet, ldm.vae, ldm.text_encoder]
+module_names = ["unet", "vae", "text_encoder"]
 num_params = sum(
     param.numel() for module in modules for param in module.parameters()
 )
+print(f"Number of parameters: {num_params / 1e6:.2f}M")
+
+for module_name, module in zip(module_names, modules):
+    num_params = sum(param.numel() for param in module.parameters())
+    print("Number of parameters in {}: {}M".format(module_name, num_params / 1e6))
 
 # run pipeline in inference (sample random noise and denoise)
 prompt = "A painting of a squirrel eating a burger"
